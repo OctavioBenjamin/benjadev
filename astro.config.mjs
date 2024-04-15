7f4459825294f58173bca62cc0b14d3d1d2cc910
Octavio Mendez <97177@sistemas.frc.utn.edu.ar>
2024-04-14 21:33:27 -0300

    Add Animations buttons Add sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
-
+import animations from "@midudev/tailwind-animations"
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: [animations]
+});
