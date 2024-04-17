df52e49745d40ad60194d6e7023b79f0051c66a7
OctavioBenjamin <benjaminmen55@gmail.com>
2024-04-17 13:01:05 -0300

    add animations add float button fix navbar del google analytics change favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-import animations from "@midudev/tailwind-animations"
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    plugins: [animations]
+  integrations: [tailwind()]
 });
